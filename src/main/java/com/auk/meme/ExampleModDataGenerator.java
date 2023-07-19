9c69926c2c337c26afe38494c1bdb711aae547c9
BuildTools <unconfigured@null.spigotmc.org>
2023-07-20 02:04:44 +0800

    Initial commit

---
+++ src/main/java/com/auk/meme/ExampleModDataGenerator.java
@@ -0,0 +1,11 @@
+package com.auk.meme;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class ExampleModDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
