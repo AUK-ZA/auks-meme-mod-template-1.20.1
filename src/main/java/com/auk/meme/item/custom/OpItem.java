078a8e4a4037096953126719c57dcefaaa37430c
BuildTools <unconfigured@null.spigotmc.org>
2023-07-21 02:01:34 +0800

    原神 启动！

--- src/main/java/com/auk/meme/item/custom/OpItem.java
+++ src/main/java/com/auk/meme/item/custom/OpItem.java
@@ -17,10 +17,11 @@ public class OpItem extends Item {
 
     @Override
     public TypedActionResult<ItemStack> use(World world, PlayerEntity playerEntity, Hand hand) {
+
         playerEntity.playSound(ModSounds.OP_SOUND_EVENT, 1.0F, 1.0F);
-        for (int i = 0; i < 5; i++) {
-            playerEntity.sendMessage(Text.translatable("原神 启动！"));//这里每循环一次会执行两次 不知道为什么
-        }
+
+        playerEntity.sendMessage(Text.of("原神 启动！"));//这里每循环一次会执行两次 不知道为什么
+
 
         return TypedActionResult.success(playerEntity.getStackInHand(hand));
     }
